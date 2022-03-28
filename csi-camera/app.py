46f0b1e28f4ae43b5acbed4574e9d623c22c8fbc
Steve Griset <steve@alwaysai.co>
2022-03-28 11:08:27 -0700

    CSI application commit - IMX477 Camera + CUDA
    MobilenetSSD v1 using CUDA backend using IMX477 csi camera

---
+++ csi-camera/app.py
@@ -0,0 +1,77 @@
+import cv2
+import time
+import datetime
+import edgeiq
+"""
+Application use the Jetson CSI IMX477 interface using the JetsonVideoStream APIs.
+Information about JetsonVideoStream can befound in alwaysai documentation at this address:
+https://alwaysai.co/docs/edgeiq_api/video_stream.html#edgeiq.edge_tools.JetsonVideoStream
+Information on the Object Detection API found in alwaysai documentation at this address:
+https://alwaysai.co/docs/edgeiq_api/object_detection.html.
+"""
+
+def main():
+    obj_detect = edgeiq.ObjectDetection("alwaysai/ssd_mobilenet_v1_coco_2018_01_28")
+    obj_detect.load(engine=edgeiq.Engine.DNN_CUDA, accelerator=edgeiq.Accelerator.NVIDIA)
+
+    print("Engine: {}".format(obj_detect.engine))
+    print("Accelerator: {}\n".format(obj_detect.accelerator))
+    print("Model:\n{}\n".format(obj_detect.model_id))
+    print("Labels:\n{}\n".format(obj_detect.labels))
+
+    fps = edgeiq.FPS()
+
+    try:
+        with edgeiq.JetsonVideoStream(cam=0,
+                                      rotation=edgeiq.FrameRotation.ROTATE_180,
+                                      camera_mode=edgeiq.
+                                      JetsonCameraMode.
+                                      IMX477_1920x1080_60_1) as video_stream, \
+                edgeiq.Streamer() as streamer:
+            # Allow Webcam to warm up
+            time.sleep(2.0)
+            fps.start()
+
+            # loop detection
+            while True:
+                frame = video_stream.read()
+                frame = edgeiq.resize(frame, width=300)
+                results = obj_detect.detect_objects(frame, confidence_level=.5)
+                frame = edgeiq.markup_image(
+                        frame, results.predictions, show_confidences=False,
+                        colors=obj_detect.colors)
+
+                # Print date and time on frame
+                current_time_date = str(datetime.datetime.now())
+                (h, w) = frame.shape[:2]
+                cv2.putText(
+                        frame, current_time_date, (10, h - 5),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+                # Generate text to display on streamer
+                text = ["Model: {}".format(obj_detect.model_id)]
+                text.append(
+                        "Inference time: {:1.3f} s".format(results.duration))
+                text.append("Objects:")
+
+                for prediction in results.predictions:
+                    text.append("{}: {:2.2f}%".format(
+                    prediction.label, prediction.confidence * 100))
+
+                streamer.send_data(frame, text)
+
+                fps.update()
+
+                if streamer.check_exit():
+                    break
+
+    finally:
+        fps.stop()
+        print("elapsed time: {:.2f}".format(fps.get_elapsed_seconds()))
+        print("approx. FPS: {:.2f}".format(fps.compute_fps()))
+
+        print("Program Ending")
+
+
+if __name__ == "__main__":
+    main()
